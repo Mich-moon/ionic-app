b2543668ac1a16e98f0b75b5c2d40daf5ec030c5
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2022-07-07 03:29:18 -0500

    list

--- myApp/src/pages/Tab2.tsx
+++ myApp/src/pages/Tab2.tsx
@@ -1,22 +1,34 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonList, IonListHeader, IonItem } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+
+import majors from "../data/majors.json";
+import minors from "../data/minors.json";
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Undergraduate courses</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Programmes</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
+        <IonList>
+          <IonListHeader>
+            Majors
+          </IonListHeader>
+          <IonItem *ngFor="let major of majors">
+            {{major.title}}
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
